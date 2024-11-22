b50f474cd920162da1e8a54bbd6b23a9d705e37a
mubasir umbi <mubasirumbi@gmail.com>
2024-11-22 23:47:35 +0530

    tooltip udated

--- modules/room/components/toolbar/Tooltip.tsx
+++ modules/room/components/toolbar/Tooltip.tsx
@@ -7,7 +7,7 @@ const Tooltip = ({ title, children }: { title: string; children: ReactNode }) =>
       <div
         className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 px-2 text-xs py-1 font-small text-[#D4D4D4] bg-[#3C3C3C] rounded-lg shadow-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity"
       >
-        {title}
+        <p className='w-auto'>{title}</p>
       </div>
     </div>
   )
